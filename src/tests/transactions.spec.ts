f63ad472750b209971852e1eeb7792702aeb2015
AdrianKnapp <adrknapp@gmail.com>
2023-05-10 08:18:04 -0300

    added list transactions test

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -14,10 +14,34 @@ describe('transaction routes', () => {
   it('should be able to create a new transaction', async () => {
     const response = await request(app.server).post('/transactions').send({
       title: 'Salary',
-      type: 'credit',
       amount: 3000,
+      type: 'credit',
     })
 
     expect(response.statusCode).toBe(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionsResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Salary',
+        amount: 3000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionsResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Salary',
+        amount: 3000,
+      }),
+    ])
   })
 })
